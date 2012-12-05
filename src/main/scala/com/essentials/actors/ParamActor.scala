6ccd2f480f451c9229d3af6ed837c9159662020a
Andrzej Kaminski <andrzej.st.kaminski@gmail.com>
2012-12-05 21:53:18 +0100

    some tests 2

--- src/main/scala/com/essentials/actors/ParamActor.scala
+++ src/main/scala/com/essentials/actors/ParamActor.scala
@@ -3,6 +3,6 @@ package com.essentials.actors
 import akka.actor.{Props, Actor}
 
 class ParamActor(age: Int) extends Actor {
-  val myWorker = context.actorOf(Props[MyActor], "Inside Param Actor")
+  val myWorker = context.actorOf(Props[MyActor], "InsideParamActor")
   protected def receive = null
 }
--- src/main/scala/com/essentials/actors/Tester.scala
+++ src/main/scala/com/essentials/actors/Tester.scala
@@ -2,8 +2,8 @@ package com.essentials.actors
 
 import akka.actor.{Props, ActorSystem}
 
-object Tester {
-  val system = ActorSystem("MyActor System")
+object Tester extends App {
+  val system = ActorSystem("MyActorSystem")
   val myActor = system.actorOf(Props[MyActor], name = "myActor")
 
   val pActor = system.actorOf(Props(new ParamActor(30)), name = "pActor")
